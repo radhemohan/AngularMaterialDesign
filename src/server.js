bcf5874d7513c376c9af603b24fb63ee5add9206
Radhemohan Dubey <lrfreelancer.mapp@gmail.com>
2018-10-03 10:22:06 +0530

    added

--- src/server.js
+++ src/server.js
@@ -8,11 +8,19 @@ and  "engines": {
   },
 */
 // server.js
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/AngularMaterialDesign'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/AngularMaterialDesign/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
