c8fcfb05b546dee37366ed17979ecab95ef2a39e
Radhemohan Dubey <lrfreelancer.mapp@gmail.com>
2018-10-06 01:27:16 +0530

    added

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const http = require('http')
 const path = require('path');
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/AngularMaterialDesign/index.html'));
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('running'));
+
+app.use(express.static(path.join(__dirname, 'dist/AngularMaterialDesign')));
+app.all('/*', function(req, res, next) {
+  // Just send the index.html for other files to support HTML5Mode
+  res.sendFile('index.html', { root: __dirname });
+});
+
+/*app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/AngularMaterialDesign/index.html'));
+});*/
+
