df718e73ca738ebc7325084540219d2112477e71
Radhemohan Dubey <lrfreelancer.mapp@gmail.com>
2018-10-06 18:54:38 +0530

    added Api routes for JOS API

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const server = http.createServer(app);
 server.listen(port, () => console.log('running'));
 
 app.use(express.static(path.join(__dirname, 'dist/AngularMaterialDesign')));
+
+app.route('/api/cats').get((req, res) => {
+ //console.log(req.get('Content-Type'));
+
+  res.send({
+    cats: [{ name: 'lilly' }, { name: 'lucy' }]
+  });
+});
 app.all('/*', function(req, res, next) {
   // Just send the index.html for other files to support HTML5Mode
   res.sendFile('index.html', { root: __dirname });
@@ -18,4 +26,6 @@ app.all('/*', function(req, res, next) {
 /*app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/AngularMaterialDesign/index.html'));
 });*/
+
+
 
