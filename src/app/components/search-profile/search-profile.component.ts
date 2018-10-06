9653d72bf92f555f030c4882e59a8cb06b273382
Radhemohan Dubey <lrfreelancer.mapp@gmail.com>
2018-10-06 12:02:25 +0530

    added

--- src/app/components/search-profile/search-profile.component.ts
+++ src/app/components/search-profile/search-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-search-profile',
@@ -6,25 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-profile.component.css']
 })
 export class SearchProfileComponent implements OnInit {
+  @ViewChild(MatPaginator) 
+  paginator: MatPaginator;
   profileType:string;
   profileTypes: string[] = ['Male', 'Female'];
   displayedColumns: string[] = ['name', 'dob', 'mob', 'address'];
   profiles:Profile[] = [{ name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
   { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-  { name:'Vijay',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
-]
+  { name:'Suresh',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Meena',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Deepa',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Ramesh',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Dinesh',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Priya',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Yogesh',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Ram',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Vivek',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+  { name:'Gaytri',  dob:'20-06-1992', mob:'8987453423',  address:'', biodata:''},
+];
+dataSource = new MatTableDataSource<Profile>(this.profiles);
   constructor() { }
 
   ngOnInit() {
+  }
+  /**
+   * Set the paginator after the view init since this component will
+   * be able to query its view for the initialized paginator.
+   */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
